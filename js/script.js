1bdd7545c32a79610eeb56c95ab70a7bb24386a3
anngi001 <anngi.alvarado@gmail.com>
2018-11-02 15:09:42 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    
+    $( "#form-login").submit(function( event ) {
+        event.preventDefault();
+        var user = $('#user').val();
+        var pass = $('#password').val();
+        
+        if (user == 'user' && pass == 'user') {
+            window.location = 'view/home.html';
+        } else {
+            if (user == 'admin' && pass == 'admin') {
+                window.location = 'admin/admin.html';
+            } else {
+            alert('El usuario no exite');
+        
+        }}
+        
+    });
+
+    $('#close').click(function(){
+        window.location = 'index.html';
+    });
+});
